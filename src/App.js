6696ba3b95373fdb2b3f0a72ba26937020ad5970
Tasneemmimi <102543325+Tasneemmimi@users.noreply.github.com>
2022-03-29 00:00:24 +0700

    new

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import  './App.css';
+const  App = () =>{
+ return( 
+ <div className="App">
+  <p className="myStyle">This is the link style</p>
+  </div>
+  )
+}
+export default App;
